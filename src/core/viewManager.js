8cd33a56e291cb2a87699f70a3a6092d6f52736d
Niloy Mondal <niloy.mondal84@gmail.com>
2013-08-24 22:36:00 +0530

    ViewManager class implemented

--- src/core/viewManager.js
+++ src/core/viewManager.js
@@ -5,6 +5,10 @@
     this.views = {};
     this.currentView = null;
     this.currentViewName = null;
+    this.face = "front";
+    this.frontSide = document.getElementById("front");
+    this.backSide = document.getElementById("back");
+    this.card = document.getElementById("card");
 
     var viewMatch = window.location.search.match(/view=([a-z0-9]+)/i);
     var viewName = "index";   // Default view = index
@@ -13,37 +17,42 @@
       viewName = viewMatch[1];
     }
 
-    Neo.Loader.loadPackage(viewName, function() {
-      var view = this._createInstanceAndAttachViewToDOM(viewName);
-      this.currentView = view;
-      this.currentViewName = viewName;
-      this.views[viewName] = view;
-    }.bind(this));
+    this.loadView(viewName);
   };
 
   Neo.Classes.ViewManager.prototype = {
     loadView: function(viewName, success) {
       var successCb = success || function() {};
+      var self = this;
 
       if (this.currentViewName === viewName) {
         successCb();
         return;
       }
 
+      function switchView(newViewName) {
+        self.card.classList.toggle("flipped");
+        self.currentViewName = newViewName;
+        self.currentView = self.views[newViewName];
+      }
+
+      this.face = this.face === "front" ? "back" : "front";
+
       if (viewName in this.views) {
-        this.currentView.hide();
-        this.views[viewName].show();
-        this.currentView = this.views[viewName];
-        this.currentViewName = viewName;
-        successCb();
+        if (this.face === "front") {
+          this.frontSide.removeChild(this.frontSide.childNodes[0]);
+          this.frontSide.appendChild(this.views[viewName].dom);
+        } else {
+          this.backSide.removeChild(this.backSide.childNodes[0]);
+          this.backSide.appendChild(this.views[viewName].dom);
+        }
+
+        switchView(viewName);
       } else {
         Neo.Loader.loadPackage(viewName, function() {
-          this.currentView.hide();
-          var view = this._createInstanceAndAttachViewToDOM(viewName);
-          this.currentView = view;
-          this.currentViewName = viewName;
+          var view = this._createInstanceAndAttachViewToDOM(viewName, this.face);
           this.views[viewName] = view;
-          successCb();
+          switchView(viewName);
         }.bind(this));
       }
     },
@@ -56,13 +65,13 @@
       return this.currentViewName;
     },
 
-    _createInstanceAndAttachViewToDOM: function(viewName) {
+    _createInstanceAndAttachViewToDOM: function(viewName, side) {
       // capitalize the first letter of the view name
       var className = viewName.charAt(0).toUpperCase() + viewName.substr(1);
 
       return Neo.createComponent({
         name: className,
-        parentDom: Neo.viewContainer
+        parentDom: side === "front" ? this.frontSide : this.backSide
       });
     }
   };
